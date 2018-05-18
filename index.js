a2c6a6d1fdb2249f71b9d8b8c312b2e1879cc598
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-18 20:03:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("partial-template").innerHTML)
+
+  Handlebars.registerHelper('displayIngredient', function() {
+    return new Handlebars.SafeString("<p>" + this + "</p>")
+  })
 }
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  let recipeName = document.getElementsByName("name")[0].value
+  let recipeDescription = document.getElementsByName("description")[0].value
+  let recipeIngredients = document.getElementsByName("ingredient").value
+  
+}
